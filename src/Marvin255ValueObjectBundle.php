1c634d8d16bc959025e39baea5910a32295a3597
marvin255 <postbox433@gmail.com>
2025-08-24 19:59:13 +0200

    Bundle structure (#1)
    * Add initial project structure with configuration files and Docker setup
    
    * Update README.md to reflect correct bundle name and links

---
+++ src/Marvin255ValueObjectBundle.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\DoctrineTranslationBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * Bundle object.
+ *
+ * @psalm-api
+ */
+final class Marvin255ValueObjectBundle extends AbstractBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    #[\Override]
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    #[\Override]
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $bundles = $builder->getParameter('kernel.bundles');
+
+        if (!\is_array($bundles) || !isset($bundles['DoctrineBundle'])) {
+            return;
+        }
+
+        $builder->prependExtensionConfig(
+            'doctrine',
+            [
+                'dbal' => [
+                    'types' => [
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    #[\Override]
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
